479000bd38f6c49d4d3c4759bb52e91322e03ee4
Zhang Peng <forbreak@163.com>
2019-01-17 10:53:34 +0800

    Demo: NG-ZORRO for Angular

---
+++ demos/ui/ng-antd-demo/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { registerLocaleData } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import zh from '@angular/common/locales/zh';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+registerLocaleData(zh);
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgZorroAntdModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
+  ],
+  providers: [{provide: NZ_I18N, useValue: zh_CN}],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
