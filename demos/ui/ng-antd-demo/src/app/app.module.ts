4e18f0febb351cc33e76dba120f837dd66a7c00d
Zhang Peng <forbreak@163.com>
2019-01-17 15:07:14 +0800

    引入ng-alain脚手架

--- demos/ui/ng-antd-demo/src/app/app.module.ts
+++ demos/ui/ng-antd-demo/src/app/app.module.ts
@@ -1,16 +1,92 @@
+// register angular
 import { registerLocaleData } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import zh from '@angular/common/locales/zh';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+// #region Http Interceptors
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
+// #region default language
+// 参考：https://ng-alain.com/docs/i18n
+import { default as ngLang } from '@angular/common/locales/zh';
+import { APP_INITIALIZER, Injector, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
-
-import { AppRoutingModule } from './app-routing.module';
+import { I18NService } from '@core/i18n/i18n.service';
+import { DefaultInterceptor } from '@core/net/default.interceptor';
+// #region Startup Service
+import { StartupService } from '@core/startup/startup.service';
+import { SimpleInterceptor } from '@delon/auth';
+import { ALAIN_I18N_TOKEN, DELON_LOCALE, zh_CN as delonLang } from '@delon/theme';
+// #endregion
+// #region i18n services
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+// #region JSON Schema form (using @delon/form)
+import { JsonSchemaModule } from '@shared/json-schema/json-schema.module';
+import { NZ_I18N, zh_CN as zorroLang } from 'ng-zorro-antd';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+import { DelonModule } from './delon.module';
+import { LayoutModule } from './layout/layout.module';
+import { RoutesModule } from './routes/routes.module';
+import { SharedModule } from './shared/shared.module';
 
-registerLocaleData(zh);
+const LANG = {
+  abbr: 'zh',
+  ng: ngLang,
+  zorro: zorroLang,
+  delon: delonLang,
+};
+registerLocaleData(LANG.ng, LANG.abbr);
+const LANG_PROVIDES = [
+  {provide: LOCALE_ID, useValue: LANG.abbr},
+  {provide: NZ_I18N, useValue: LANG.zorro},
+  {provide: DELON_LOCALE, useValue: LANG.delon},
+];
+
+// 加载i18n语言文件
+export function I18nHttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, `assets/tmp/i18n/`, '.json');
+}
+
+const I18NSERVICE_MODULES = [
+  TranslateModule.forRoot({
+    loader: {
+      provide: TranslateLoader,
+      useFactory: I18nHttpLoaderFactory,
+      deps: [HttpClient]
+    }
+  })
+];
+
+const I18NSERVICE_PROVIDES = [
+  {provide: ALAIN_I18N_TOKEN, useClass: I18NService, multi: false}
+];
+// #region
+const FORM_MODULES = [JsonSchemaModule];
+// #endregion
+const INTERCEPTOR_PROVIDES = [
+  {provide: HTTP_INTERCEPTORS, useClass: SimpleInterceptor, multi: true},
+  {provide: HTTP_INTERCEPTORS, useClass: DefaultInterceptor, multi: true}
+];
+// #endregion
+
+// #region global third module
+const GLOBAL_THIRD_MODULES = [];
+
+// #endregion
+export function StartupServiceFactory(startupService: StartupService): Function {
+  return () => startupService.load();
+}
+
+const APPINIT_PROVIDES = [
+  StartupService,
+  {
+    provide: APP_INITIALIZER,
+    useFactory: StartupServiceFactory,
+    deps: [StartupService],
+    multi: true
+  }
+];
+
+// #endregion
 
 @NgModule({
   declarations: [
@@ -18,13 +94,23 @@ registerLocaleData(zh);
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgZorroAntdModule,
-    FormsModule,
+    BrowserAnimationsModule,
     HttpClientModule,
-    BrowserAnimationsModule
+    DelonModule.forRoot(),
+    CoreModule,
+    SharedModule,
+    LayoutModule,
+    RoutesModule,
+    ...I18NSERVICE_MODULES,
+    ...FORM_MODULES,
+    ...GLOBAL_THIRD_MODULES
   ],
-  providers: [{provide: NZ_I18N, useValue: zh_CN}],
+  providers: [
+    ...LANG_PROVIDES,
+    ...INTERCEPTOR_PROVIDES,
+    ...I18NSERVICE_PROVIDES,
+    ...APPINIT_PROVIDES
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
